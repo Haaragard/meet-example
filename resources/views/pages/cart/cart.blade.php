152d08267cd9afe4e5acf007f1108a92587d4fe5
Diogo Antunes <diogololz159@gmail.com>
2021-08-12 02:12:46 -0300

    Added cart page view

---
+++ resources/views/pages/cart/cart.blade.php
@@ -0,0 +1,11 @@
+<x-layouts.app>
+    <x-slot name="title">Página principal</x-slot>
+
+    <div class="bg-gray-50">
+        <h1>Cart</h1>
+
+        <div class="mt-5" style="min-height: 300px;">
+            <livewire:forms.cart />
+        </div>
+    </div>
+</x-layouts.app>
